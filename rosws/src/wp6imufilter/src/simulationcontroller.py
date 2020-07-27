09768fb65ad302d49f5316c4b30554ab4c9716d6
Alexander Bauer <alexander.bauer.1@campus.tu-berlin.de>
2020-07-27 14:22:08 +0200

    added reset cube function to controller node

--- rosws/src/wp6imufilter/src/simulationcontroller.py
+++ rosws/src/wp6imufilter/src/simulationcontroller.py
@@ -1,7 +1,8 @@
 #!/bin/python
 import rospy
 from std_srvs.srv import Empty, EmptyResponse
-from gazebo_msgs.srv import SetModelConfiguration, SetModelConfigurationRequest, SetModelConfigurationResponse
+from gazebo_msgs.srv import SetModelConfiguration, SetModelConfigurationRequest, SetModelConfigurationResponse, SetModelState
+from gazebo_msgs.msg import ModelState
 import time
 import Tkinter as tk
 
@@ -9,11 +10,13 @@ class ControllerGUI(tk.Frame):
     def __init__(self,controller_node, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         self.controller_node = controller_node
+
         self.pause_button = tk.Button(text='pause', command = controller_node._pause_physics_client)
         self.unpause_button = tk.Button(text='unpause', command = controller_node._unpause_physics_client)
+        self.reset_button = tk.Button(text='reset', command = controller_node._set_cube_state_client)
         self.pause_button.pack()
         self.unpause_button.pack()
-
+        self.reset_button.pack()
 
 class SimulationController(object):
     #Class for simulation control
@@ -21,32 +24,40 @@ class SimulationController(object):
         self.name = name
         self.objectname = objectname
 
+        self.modelstate = ModelState()
+        self.modelstate.model_name = 'unit_box'
+        self.modelstate.pose.position.x = 0
+        self.modelstate.pose.position.y = 0
+        self.modelstate.pose.position.z = 5
+        self.modelstate.pose.orientation.x = 0
+        self.modelstate.pose.orientation.y = 0
+        self.modelstate.pose.orientation.z = 0
+        self.modelstate.pose.orientation.w = 0
         rospy.init_node(self.name)
 
-        #wait for service to be available
+        #wait for services to be available
         rospy.loginfo('waiting for /gazebo/pause_physics service... (start gazebo via "rosrun gazebo_ros gazebo")')
         rospy.wait_for_service('/gazebo/pause_physics')
         rospy.loginfo('The service \'/gazebo/pause_physics\' has been found')
 
+        rospy.loginfo('waiting for /gazebo/set_model_state service... (start gazebo via "rosrun gazebo_ros gazebo")')
+        rospy.wait_for_service('/gazebo/set_model_state')
+        rospy.loginfo('The service \'/gazebo/set_model_state\' has been found')
+
         self.pause_physics_client = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.unpause_physics_client = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
-
-    
-    # def run(self):
-    #     rospy.loginfo('Starting simulation controller')
-    #     while not rospy.is_shutdown():
-    #         self._unpause_physics_client()
-    #         self._pause_physics_client()
+        self.set_cube_state_client = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
     def _pause_physics_client(self):
         self.pause_physics_client()
         rospy.loginfo('Pause request has been sent')
-        # time.sleep(4)
     
     def _unpause_physics_client(self):
         self.unpause_physics_client()
         rospy.loginfo('Unpause request has been sent')
-        # time.sleep(4)
+
+    def _set_cube_state_client(self):
+        self.set_cube_state_client(self.modelstate)
 
 if __name__ == '__main__':
     
