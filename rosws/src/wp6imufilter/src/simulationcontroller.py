969b8bc794ec4e9d75c2dd81c619c25be1b9a03e
Alexander Bauer <alexander.bauer.1@campus.tu-berlin.de>
2020-07-28 17:43:41 +0200

    added graph for later data-viz

--- rosws/src/wp6imufilter/src/simulationcontroller.py
+++ rosws/src/wp6imufilter/src/simulationcontroller.py
@@ -9,19 +9,46 @@ import Tkinter as tk
 import tkFileDialog
 import csv
 
+import threading
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
 class ControllerGUI(tk.Frame):
     def __init__(self,controller_node, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        self.controller_node = controller_node
+        controller_node.GUI = self
+        controller_node._pause_physics_client()
+        
+        #matplotlib canvas:
+        self.handle = None
+        self.mpl_frame = tk.Frame(master = self)
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.ax.plot([0,0], [0,0])
+        self.canvas = FigureCanvasTkAgg(self.fig, master = self.mpl_frame)
+        self.canvas.draw()
+        self.toolbar = NavigationToolbar2TkAgg(self.canvas, self.mpl_frame)
+        self.toolbar.update()
+        self.mpl_frame.pack(side = tk.TOP, fill = tk.BOTH, expand = 1)
 
-        self.pause_button = tk.Button(text='pause', command = self.controller_node._pause_physics_client)
-        self.unpause_button = tk.Button(text='unpause', command = self.controller_node._unpause_physics_client)
-        self.reset_button = tk.Button(text='reset', command = self.controller_node._reset_cube)
-        self.save_button = tk.Button(text='save to .CSV', command = self.save_data)
-        self.pause_button.pack(fill = tk.BOTH)
-        self.unpause_button.pack(fill = tk.BOTH)
-        self.reset_button.pack(fill = tk.BOTH)
-        self.save_button.pack(fill = tk.BOTH)
+
+
+        self.controller_node = controller_node
+        self.pause_button = tk.Button(master = self, text='pause', command = self.controller_node._pause_physics_client)
+        self.unpause_button = tk.Button(master = self, text='unpause', command = self.controller_node._unpause_physics_client)
+        self.reset_button = tk.Button(master = self, text='reset', command = self.controller_node._reset_cube)
+        self.save_button = tk.Button(master = self, text='save to .CSV', command = self.save_data)
+        
+        # self.toolbar.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.pause_button.pack(fill = tk.BOTH, side = tk.BOTTOM)
+        self.unpause_button.pack(fill = tk.BOTH, side = tk.BOTTOM)
+        self.reset_button.pack(fill = tk.BOTH, side = tk.BOTTOM)
+        self.save_button.pack(fill = tk.BOTH, side = tk.BOTTOM)
+        self.pack(fill = tk.BOTH)
+        controller_node._reset_cube()
     
     def save_data(self):
         dir = tkFileDialog.asksaveasfilename(title = 'select place to save', defaultextension = '.csv')
@@ -56,11 +83,31 @@ class ControllerGUI(tk.Frame):
                     csvwriter.writerow([seq,ts,tns,ox,oy,oz,ow, avx,avy,avz,lax,lay,laz])
                 print('done')
 
-
+    def plot_data(self,data_list):
+        laz = []
+        ts = []
+        for data in data_list:
+            ts.append(data.header.stamp.secs)
+            # avx = data.angular_velocity.x
+            # avy = data.angular_velocity.y
+            # avz = data.angular_velocity.z
+            # lax = data.linear_acceleration.x
+            # lay = data.linear_acceleration.y
+            laz.append(data.linear_acceleration.z)
+        if self.handle != None:
+            self.handle.remove()
+        [self.handle] = self.ax.plot(ts, laz, linestyle = '-', color = 'b', marker = '.')
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.ax.set_xlim(xmin = ts[0], xmax = ts[-1])
+        self.canvas.draw()
+        return
+        
 class SimulationController(object):
     #Class for simulation control
-    def __init__(self, name='simulationcontroller', objectname='unit_box'):
+    def __init__(self, GUI = None, name='simulationcontroller', objectname='unit_box'):
         self.name = name
+        self.GUI = GUI
         self.objectname = objectname
         self.data_list = []
         self.modelstate = ModelState()
@@ -87,7 +134,17 @@ class SimulationController(object):
         self.unpause_physics_client = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.set_cube_state_client = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
         self.sub = rospy.Subscriber('/imu', ImuMsg, callback=self._imu_topic_callback)
+        
+        self.plot_rate = rospy.Rate(0.5)
+        self.update_plot_thread = threading.Thread(target = self.update_plot)
+        self.update_plot_thread.daemon = True
+        self.update_plot_thread.start()
 
+    def update_plot(self):
+        while not rospy.is_shutdown():
+            self.plot_rate.sleep()
+            self.GUI.plot_data(self.data_list)
+        
     def _imu_topic_callback(self,data):
         rospy.loginfo('Received IMU msg: {0}'.format(data))
         self.data_list.append(data)
@@ -107,6 +164,8 @@ class SimulationController(object):
         '''
         self.data_list = []
         self.set_cube_state_client(self.modelstate)
+        rospy.loginfo('reset request has been sent')
+
 
 if __name__ == '__main__':
     
