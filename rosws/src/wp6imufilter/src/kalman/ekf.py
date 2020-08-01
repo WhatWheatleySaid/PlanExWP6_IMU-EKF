9fc9f997139389303d8b00116982e01fe2d93744
Ga Man Liang <g.liang@campus.tu-berlin.de>
2020-08-01 18:33:28 +0200

    implementing EKF algorithm and quaternion calculation + tools, not tested yet

--- rosws/src/wp6imufilter/src/kalman/ekf.py
+++ rosws/src/wp6imufilter/src/kalman/ekf.py
@@ -1,8 +1,10 @@
 #!/bin/python
-import csv
 import numpy as np
 from pathlib import Path
 import pandas as pd
+
+from kalman import quaternion_tools as qtool
+
 
 class EkfEstimation:
     def __init__(self, df):
@@ -10,16 +12,18 @@ class EkfEstimation:
 
         self.motion_start = 0
         self.motion_end = self.df.shape[0]
+        self.rate = 10
 
         self.gyro_bias()
 
+        self.ekf_ori_estimation()
         0
 
     def cleanup_df(self, df):
         # determine length
         n = df.angular_velocity_x.size
         df_clean = pd.DataFrame(data={
-            "time": df.time_seconds+df.time_nseconds/1e9 - df.time_seconds.iloc[0],
+            "time": df.time_seconds + df.time_nseconds / 1e9 - df.time_seconds.iloc[0],
             "gyro_x": df.angular_velocity_x,
             "gyro_y": df.angular_velocity_y,
             "gyro_z": df.angular_velocity_z,
@@ -43,11 +47,69 @@ class EkfEstimation:
         self.df.gyro_y = self.df.gyro_y - self.ds_gyrbias.gyro_y
         self.df.gyro_z = self.df.gyro_z - self.ds_gyrbias.gyro_z
 
+    def ekf_ori_estimation(self):
+        n = self.df.acc_x.shape[0]
+        quat = np.zeros((n, 4))
+        P = np.eye(4)
+        V = np.eye(4) * 0.00001
+        W = np.zeros((6, 6))
+        W[:3, :3] = np.eye(3) * 10
+        W[3:6, 3:6] = np.eye(3) * 0.01
+        ds_gyr = self.df[['gyro_x', 'gyro_y', 'gyro_z']].to_numpy()
+        ds_acc = self.df[['acc_x', 'acc_y', 'acc_z']].to_numpy()
+        ds_mag = self.df[['mag_x', 'mag_y', 'mag_z']].to_numpy()
+
+        Fx = np.zeros((4, 4))
+        for i in range(1, ds_gyr.shape[0]):
+            q_gyr = qtool.quaternion_from_gyr(ds_gyr[i - 1,], self.rate)
+            # while waiting for sim data, generate gyr quat
+            q_gyr = np.hstack(np.array(
+                [np.random.random(1), np.random.random(1), np.random.random(1), np.random.random(1)])).flatten()
+            g1, g2, g3, g4 = q_gyr
+            Fx[0, :] = [g1, -g2, -g3, -g4]
+            Fx[1, :] = [g2, g1, g4, -g3]
+            Fx[2, :] = [g3, -g4, g1, g2]
+            Fx[3, :] = [g4, g3, -g2, g1]
+            q_accmag = qtool.quaternion_from_accmag(ds_acc[i,], ds_mag[i,])
+
+            # while waiting for sim data, generate accmag quat
+            q_accmag = np.hstack(np.array(
+                [np.random.random(1), np.random.random(1), np.random.random(1), np.random.random(1)])).flatten()
+            x_hat_pre = qtool.quaternion_multiply(quat[i - 1,], q_gyr)
+
+            # compute Hesse matrix
+            q1, q2, q3, q4 = x_hat_pre
+            h1 = np.zeros((3, 4))
+            h2 = np.zeros((3, 4))
+            h1[0, :] = [-q3, q4, -q1, q2]
+            h1[1, :] = [q2, q1, q4, q3]
+            h1[2, :] = [q1, -q2, -q3, q4]
+            h1 = h1 * 2 * 9.81
+
+            h2[0, :] = [q4, q3, q2, q1]
+            h2[1, :] = [q1, -q2, q3, -q4]
+            h2[2, :] = [-q2, -q1, -q4, q3]
+            hx1 = qtool.quaternion_rotate(qtool.quaternion_invert(x_hat_pre.T), np.array([0, 0, 9.8]))
+            hx2 = qtool.quaternion_rotate(qtool.quaternion_invert(x_hat_pre.T), np.array([0, 1, 0]))
+
+            # EKF algorithm
+            p_pre = Fx * P * Fx.T + V
+            H = np.vstack([h1, h2])
+            # kalman gain
+            K = p_pre.dot(H.T).dot(np.linalg.inv(H.dot(p_pre.dot(H.T)) + W))
+            x_hat = x_hat_pre + K.dot((np.vstack([ds_acc[i,], ds_mag[i, ]]).flatten() -
+                                     np.vstack([hx1, hx2]).flatten()).T)
+            # update P
+            P = (np.eye(4) - K.dot(H)).dot(p_pre.dot((np.eye(4) - K.dot(H)).T)) + K.dot(W.dot(K.T))
+            # update quat
+            quat[i,] = x_hat.T
+        self.quat = quat
+
+
 
 if __name__ == "__main__":
     fpath = Path("kalman/data")
     df = pd.read_csv(fpath / "test_ds.csv", sep="\t")
-
 
     EkfEstimation(df)
     0
---
+++ rosws/src/wp6imufilter/src/kalman/quaternion_tools.py
@@ -0,0 +1,84 @@
+import numpy as np
+from numpy.linalg import norm
+import math as math
+
+def quaternion_from_gyr(gyr, rate):
+    """
+    compute quaternion from single gyro data (x,y,z).
+    """
+
+    gyrnorm = norm(gyr)
+    axis = gyr/gyrnorm
+    angle = gyrnorm * (1/rate)
+    quat_curr_prev = np.zeros(4)
+    quat_curr_prev[0] = np.cos(angle/2)
+    quat_curr_prev[1:] = axis * np.sin(angle/2)
+    return quat_curr_prev
+
+def quaternion_from_accmag(acc, mag):
+    """
+    compute quaternion from single acc & mag data (x,y,z).
+    """
+
+    z_earth_imu = acc/norm(acc)
+    x_earth_imu = np.cross(mag, z_earth_imu)/norm(np.cross(mag, z_earth_imu))
+    y_earth_imu = np.cross(z_earth_imu,x_earth_imu)/norm(np.cross(z_earth_imu, x_earth_imu))
+
+    quat_imu_earth = quaternion_from_rotmat(np.array([x_earth_imu.T, y_earth_imu.T, z_earth_imu.T]))
+    return quat_imu_earth
+
+
+def quaternion_from_rotmat(r):
+    """
+    Creates quaternion from a rotation matrix.
+    :param r: rotation matrix
+    :type r: ndarray
+    :return: q
+    """
+    w_sq = (1 + r[0,0] + r[1,1] + r[2,2])/4
+    x_sq = (1 + r[0,0] - r[1,1] - r[2,2])/4
+    y_sq = (1 - r[0,0] + r[1,1] - r[2,2])/4
+    z_sq = (1 - r[0,0] - r[1,1] + r[2,2])/4
+
+    w = np.sqrt(w_sq)
+    x = math.copysign(np.sqrt(x_sq), r[2,1]-r[1,2])
+    y = math.copysign(np.sqrt(y_sq), r[0,2]-r[2,0])
+    z = math.copysign(np.sqrt(z_sq), r[1,0]-r[0,1])
+    return np.array([w, x, y, z])
+
+def quaternion_multiply(q1, q2):
+    """
+    Multiplies quaternion, returns quaternion with same shape. q1 and q2 must have shape 4x1
+    :param q1: ndarray(4)
+    :param q2: ndarray(4)
+    :return q3: ndarray(4)
+    """
+    q3 = np.zeros_like(q1)
+    q3[0] = q1[0] * q2[0] - q1[1] * q2[1] - q1[2] * q2[2] - q1[3]*q2[3]
+    q3[1] = q1[0] * q2[1] - q1[1] * q2[0] - q1[2] * q2[3] - q1[3]*q2[2]
+    q3[2] = q1[0] * q2[2] - q1[1] * q2[3] - q1[2] * q2[0] - q1[3]*q2[1]
+    q3[3] = q1[0] * q2[3] - q1[1] * q2[2] - q1[2] * q2[1] - q1[3]*q2[0]
+    return q3
+
+def quaternion_rotate(q, vec):
+    """
+    Rotate the vectors v by the quaternions q
+
+    :param q:
+    :param vec:
+    :return:
+    """
+    qinv = quaternion_invert(q)
+    foo = np.hstack(np.array([0, vec])).flatten()
+
+    qv = quaternion_multiply(quaternion_multiply(q, foo), qinv)
+    v = qv[1:]
+    return v
+
+
+def quaternion_invert(q):
+    return np.hstack(np.array([q[0], -q[1:]])).flatten()
+
+
+
+
