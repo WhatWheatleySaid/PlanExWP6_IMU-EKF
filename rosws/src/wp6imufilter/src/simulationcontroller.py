94a9b4f360af207bd57191605d873e4adf74fcc7
Alexander Bauer <alexander.bauer.1@campus.tu-berlin.de>
2020-08-03 18:34:43 +0200

    added reading/saving of magnetic topic

--- rosws/src/wp6imufilter/src/simulationcontroller.py
+++ rosws/src/wp6imufilter/src/simulationcontroller.py
@@ -4,6 +4,7 @@ from std_srvs.srv import Empty, EmptyResponse
 from gazebo_msgs.srv import SetModelConfiguration, SetModelConfigurationRequest, SetModelConfigurationResponse, SetModelState
 from gazebo_msgs.msg import ModelState
 from sensor_msgs.msg import Imu as ImuMsg
+from geometry_msgs.msg import Vector3Stamped as MagneticMessage
 import time
 import Tkinter as tk
 import tkMessageBox as messagebox
@@ -14,6 +15,7 @@ import threading
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import numpy as np
 
 class ControllerGUI(tk.Frame):
     def __init__(self,controller_node, *args, **kwargs):
@@ -27,6 +29,7 @@ class ControllerGUI(tk.Frame):
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.ax.plot([0,0], [0,0])
+        self.ax.set_xlabel('t [s]')
         self.canvas = FigureCanvasTkAgg(self.fig, master = self.mpl_frame)
         self.canvas.draw()
         self.toolbar = NavigationToolbar2TkAgg(self.canvas, self.mpl_frame)
@@ -185,29 +188,34 @@ class ControllerGUI(tk.Frame):
                 csvwriter.writerow(['sequence', 'time_seconds', 'time_nseconds',
                                     'orientation_x', 'orientation_y','orientation_z', 'orientation_w',
                                     'angular_velocity_x','angular_velocity_y','angular_velocity_z',
-                                    'linear_acceleration_x', 'linear_acceleration_y', 'linear_acceleration_z'])
+                                    'linear_acceleration_x', 'linear_acceleration_y', 'linear_acceleration_z',
+                                    'mag_x', 'mag_y', 'mag_z'])
                 
-                for data in self.controller_node.data_list:
-                    seq = data.header.seq
-                    ts = data.header.stamp.secs
-                    tns = data.header.stamp.nsecs
+                for data_imu, data_mag in zip(self.controller_node.data_list_imu, self.controller_node.data_list_mag):
+                    seq = data_imu.header.seq
+                    ts = data_imu.header.stamp.secs
+                    tns = data_imu.header.stamp.nsecs
 
-                    ox = data.orientation.x
-                    oy = data.orientation.y
-                    oz = data.orientation.z
-                    ow = data.orientation.w
+                    ox = data_imu.orientation.x
+                    oy = data_imu.orientation.y
+                    oz = data_imu.orientation.z
+                    ow = data_imu.orientation.w
 
-                    avx = data.angular_velocity.x
-                    avy = data.angular_velocity.y
-                    avz = data.angular_velocity.z
+                    avx = data_imu.angular_velocity.x
+                    avy = data_imu.angular_velocity.y
+                    avz = data_imu.angular_velocity.z
 
-                    lax = data.linear_acceleration.x
-                    lay = data.linear_acceleration.y
-                    laz = data.linear_acceleration.z
-                    csvwriter.writerow([seq,ts,tns,ox,oy,oz,ow, avx,avy,avz,lax,lay,laz])
+                    lax = data_imu.linear_acceleration.x
+                    lay = data_imu.linear_acceleration.y
+                    laz = data_imu.linear_acceleration.z
+
+                    mag_x = data_mag.vector.x
+                    mag_y = data_mag.vector.y
+                    mag_z = data_mag.vector.z
+                    csvwriter.writerow([seq,ts,tns,ox,oy,oz,ow, avx,avy,avz,lax,lay,laz,mag_x,mag_y,mag_z])
                 print('done')
 
-    def plot_data(self,data_list):
+    def plot_data(self,data_list_imu, data_list_mag):
         lax = []
         lay = []
         laz = []
@@ -215,8 +223,13 @@ class ControllerGUI(tk.Frame):
         avx = []
         avy = []
         avz = []
-        
-        for data in data_list:
+        magx = []
+        magy = []
+        magz = []
+        ts_imu = []
+        ts_mag = []
+        for data in data_list_imu:
+            ts_imu.append(data.header.stamp.secs + data.header.stamp.nsecs*1e-9)
             seq.append(data.header.seq)
             avx.append(data.angular_velocity.x)
             avy.append(data.angular_velocity.y)
@@ -224,13 +237,23 @@ class ControllerGUI(tk.Frame):
             lax.append(data.linear_acceleration.x)
             lay.append(data.linear_acceleration.y)
             laz.append(data.linear_acceleration.z)
+        for data in data_list_mag:
+            ts_mag.append(data.header.stamp.secs + data.header.stamp.nsecs*1e-9)
+            magx.append(data.vector.x)
+            magy.append(data.vector.y)
+            magz.append(data.vector.z)
+
+
         plot_data = [
-            [seq, avx, 'green', 'angular-vel-x'],
-            [seq, avy, 'blue', 'angular-vel-y'],
-            [seq, avz, 'cyan', 'angular-vel-x'],
-            [seq, lax, 'black', 'linear-acc-x'],
-            [seq, lay, 'red', 'linear-acc-y'],
-            [seq, laz, 'orange', 'linear-acc-z']
+            [ts_imu, avx, 'green', 'angular-vel-x [1/s]'],
+            [ts_imu, avy, 'blue', 'angular-vel-y [1/s]'],
+            [ts_imu, avz, 'cyan', 'angular-vel-x [1/s]'],
+            [ts_imu, lax, 'black', 'linear-acc-x [m/s^2]'],
+            [ts_imu, lay, 'red', 'linear-acc-y [m/s^2]'],
+            [ts_imu, laz, 'orange', 'linear-acc-z [m/s^2]'],
+            [ts_mag, magx, 'magenta', 'mag-x'],
+            [ts_mag, magy, 'orchid', 'mag-y'],
+            [ts_mag, magx, 'darkorchid', 'mag-x']
         ]
         if self.handles:
             for handle in self.handles:
@@ -238,10 +261,9 @@ class ControllerGUI(tk.Frame):
         self.handles = []
         for pd in plot_data:
             self.handles.append(self.ax.plot(pd[0],pd[1],color = pd[2], linestyle = '-', label = pd[3]))
-        # self.ax.relim()
-        # self.ax.autoscale_view()
-        self.ax.legend()
-        self.ax.set_xlim(xmin = seq[0], xmax = seq[-1])
+        self.ax.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+        self.ax.set_xlim(xmin = ts_imu[0], xmax = ts_imu[-1])
+        self.fig.subplots_adjust(right=0.7)
         self.ax.set_ylim(ymin = -20, ymax = 20)
         self.canvas.draw()
         return
@@ -252,9 +274,9 @@ class SimulationController(object):
         self.name = name
         self.GUI = GUI
         self.objectname = objectname
-        self.data_list = []
+        self.data_list_imu = []
+        self.data_list_mag = []
         self.modelstate = ModelState()
-        print(dir(self.modelstate.twist.linear))
         self.modelstate.model_name = 'simple_cube'
         self.modelstate.pose.position.x = 0
         self.modelstate.pose.position.y = 0
@@ -288,9 +310,9 @@ class SimulationController(object):
         self.pause_physics_client = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
         self.unpause_physics_client = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.set_cube_state_client = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-        self.sub = rospy.Subscriber('/imu', ImuMsg, callback=self._imu_topic_callback)
-        
-        self.plot_rate = rospy.Rate(5)
+        self.sub_imu = rospy.Subscriber('/imu', ImuMsg, callback=self._imu_topic_callback)
+        self.sub_mag = rospy.Subscriber('/magnetic', MagneticMessage, callback=self._magnetic_topic_callback)
+        self.plot_rate = rospy.Rate(2)
         self.update_plot_thread = threading.Thread(target = self.update_plot)
         self.update_plot_thread.daemon = True
         self.update_plot_thread.start()
@@ -298,13 +320,16 @@ class SimulationController(object):
     def update_plot(self):
         while not rospy.is_shutdown():
             self.plot_rate.sleep()
-            if not rospy.is_shutdown() and self.data_list:
-                self.GUI.plot_data(self.data_list)
+            if not rospy.is_shutdown() and self.data_list_imu:
+                self.GUI.plot_data(self.data_list_imu, self.data_list_mag)
             
         
     def _imu_topic_callback(self,data):
         # rospy.loginfo('Received IMU msg: {0}'.format(data))
-        self.data_list.append(data)
+        self.data_list_imu.append(data)
+
+    def _magnetic_topic_callback(self,data):
+        self.data_list_mag.append(data)
 
     def _pause_physics_client(self):
         self.pause_physics_client()
@@ -330,7 +355,7 @@ class SimulationController(object):
         resets the cubes position and empties gathered data list
         '''
         self.pause_physics_client()
-        self.data_list = []
+        self.data_list_imu = []
         self.set_cube_state_client(self.modelstate)
         self.reset_flag = True
         rospy.loginfo('reset request has been sent')
